47e34d66fab03731f192c0075bab78dfe080484e
Katy Jendrzey <katyjendrzey@csm-wl-dhcp-236-79.mines.edu>
2019-11-18 13:40:14 -0700

    added loadfile

--- Week12-utility.py
+++ Week12-utility.py
@@ -4,3 +4,19 @@
 
 def PrintOutput(printprint):
     print('OUTPUT', printprint)
+
+def LoadFile(filename):
+    alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz '
+    alphabet = list(alphabet)
+    filename = open(filename, 'r+')
+    filename = filename.readlines()
+    newoutput = list(filename)
+    for val in range(0, len(newoutput)):
+        for index in range(0, len(newoutput[val])):
+            output = list(newoutput[val])
+            if ''.join(output[index:index +1]) == '\n':
+                del output[index:index + 1]
+            newoutput[val] = ''.join(output)
+    return newoutput
+    
+    
